1f66f2a00e78aae33f3ef1e6d189230ba0f4f4be
Andrew Lyko <marian.lyko@gmail.com>
2022-03-31 20:22:27 +0200

    add firestore and get data from it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import {useEffect, useState} from "react";
 
+import {collection, getDocs} from "firebase/firestore"
+
 import {loadFromLocalStorage, saveToLocalStorage} from "./utils/localstorage";
 import uuidGen from "./utils/uuid";
 
 import Headline from "./components/Headline";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
+import {db} from "./firebase";
 
 
 function App() {
     const [value, setValue] = useState('')
     const [tasks, setTasks] = useState([]);
     const [selection, setSelection] = useState('all');
+
+
+    const getData = async () => {
+        const querySnapshot = await getDocs(collection(db, "todos"));
+        setTasks(querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        })));
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
 
     useEffect(() => {
         setTasks(loadFromLocalStorage('tds'));
@@ -66,24 +82,25 @@ function App() {
             />
             {tasks.length === 0 ? ('') : (
                 <>
-                <TaskList
-                    tasks={tasks}
-                    handleChangeStatus={handleChangeStatus}
-                    handleDeleteTask={handleDeleteTask}
-                    selection={selection}/>
+                    <TaskList
+                        tasks={tasks}
+                        handleChangeStatus={handleChangeStatus}
+                        handleDeleteTask={handleDeleteTask}
+                        selection={selection}/>
 
                     {/*TODO move to separate component */}
                     <p> {tasks.filter((e) => !e.status).length} items left</p>
 
                     {/*TODO move to separate component */}
                     <div>
-                    <button onClick={() => setSelection('all')}>All</button>
-                    <button onClick={() => setSelection(false)}>Active</button>
-                    <button onClick={() => setSelection(true)}>Completed</button>
+                        <button onClick={() => setSelection('all')}>All</button>
+                        <button onClick={() => setSelection(false)}>Active</button>
+                        <button onClick={() => setSelection(true)}>Completed</button>
                     </div>
 
                     {/*TODO move to separate component */}
-                    {tasks.filter((e) => e.status).length > 0 ? (<button onClick={handleDeleteDone}>Clear Completed</button>) : ('')}
+                    {tasks.filter((e) => e.status).length > 0 ? (
+                        <button onClick={handleDeleteDone}>Clear Completed</button>) : ('')}
                 </>)}
         </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCfTt0u38YXuwp6_BO6okL-Jhw--knwdPM",
@@ -10,6 +11,8 @@ const firebaseConfig = {
 };
 
 
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
 
-export default app;
+export const db = getFirestore(firebase);
+
+export default firebase;
